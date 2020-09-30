9e8b046155a7ab6e9708ac9c1352f881ffb84583
M. Wildhan Juniar D.S <wildhanjuniar@gmail.com>
2020-09-30 21:02:00 +0700

    subtotal

--- src/__tests__/helpers.js
+++ src/__tests__/helpers.js
@@ -11,3 +11,7 @@ test('1 + 1 = 2', () => {
 test('2 - 1 = 1', () => {
     expect(functions.decrementQty(2)).toBe(1);
 });
+
+test('50000 x 2 = 100000', () => {
+    expect(functions.total(50000, 2)).toBe(100000);
+});
--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
-import { 
-  incrementQty, 
-  decrementQty
+import {
+  incrementQty,
+  decrementQty,
+  total
 } from './helpers.js';
 
 const incrButton = document.querySelector('#incr');
 const decrButton = document.querySelector('#decr');
 const qtyInput = document.querySelector('#qty');
+const price = document.querySelector('#price');
+const subtotal = document.querySelector('#subtotal');
 
 incrButton.addEventListener('click', () => {
   qtyInput.value = incrementQty(qtyInput.value);
+  subtotal.textContent = `Rp. ${total(price.value, qtyInput.value)}`;
 });
 
 decrButton.addEventListener('click', () => {
   qtyInput.value = decrementQty(qtyInput.value);
+  subtotal.textContent = `Rp. ${total(price.value, qtyInput.value)}`;
 });
